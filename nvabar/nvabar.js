d6bbb6f5a96409cb984d9c9d493bf91071ccce85
Le Viet <150636584+letrungviet2005@users.noreply.github.com>
2024-05-19 12:42:17 +0700

    nvabar.js

--- nvabar/nvabar.js
+++ nvabar/nvabar.js
@@ -3,11 +3,9 @@ const sidebar = document.getElementById('sidebar');
 function MenuBar() {
 overlay.style.display = 'block';
 sidebar.classList.add('open');
-container.style.display= "none";
 
 }
 function CloseBar() {
 overlay.style.display = 'none';
 sidebar.classList.remove('open');
-container.style.display= "block";
 }
