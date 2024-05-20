dff07b3823bd641d5c67eb3bd7c509d9428aada7
Le Viet <150636584+letrungviet2005@users.noreply.github.com>
2024-05-20 21:16:42 +0700

    Update TaiKhoan.js

--- Script/TaiKhoan.js
+++ Script/TaiKhoan.js
@@ -17,6 +17,18 @@ function CloseBar() {
 function navToIntro() {
     window.location.href = '../../index.html';
 }
+
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
     
 
 // navigation bar background transparency
