91b8f4581261fa36c8b24319e76313b9553922fd
Le Viet <150636584+letrungviet2005@users.noreply.github.com>
2024-05-23 22:48:59 +0700

    Update TaiKhoan.js

--- Script/TaiKhoan.js
+++ Script/TaiKhoan.js
@@ -5,16 +5,6 @@ function navToIntro() {
 }
 
 const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
-
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
 
         const overlay = document.getElementById('overlay');
         const sidebar = document.getElementById('sidebar');
